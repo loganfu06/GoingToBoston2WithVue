132679dfca3cb3eb2baabbc196eab9c557d67172
Logan Fu <lfu40@stuy.edu>
2024-02-26 14:44:52 -0500

    worked on vue project

--- main.js
+++ main.js
@@ -8,19 +8,55 @@ createApp({
             p1TotalScore: 0,
             p1RoundScore: 0,
             p1RoundState: false,
+            p1InitialDice: 0,
             // p2 variables
+            p2InitialDice: 0,
             p2TotalScore: 0,
             p2RoundScore: 0,
             p2RoundState: false,
             dice1: 0,
             dice2: 0,
             dice3: 0,
+            // round variables
+            totalRounds: 1,
+            currentRound: 0,
+            firstPlayer: 0,
+            
 
         };
     },
     methods: {
         restartGame() {
-            // do this later
+            this.gameStart = false
+            this.winner = false
+            // p1 variables
+            this.p1TotalScore = 0
+            this.p1RoundScore = 0
+            this.p1RoundState = false
+            this.p1InitialDice = 0
+
+            // p2 variables
+            this.p2TotalScore = 0
+            this.p2RoundScore = 0
+            this.p2RoundState = false
+            this.p2InitialDice = 0
+
+            this.dice1 = 0
+            this.dice2 = 0
+            this.dice3 = 0
+            // round variables
+            this.totalRounds = 1
+            this.currentRound = 0
+            this.firstPlayer = 0
+        },
+        startGame() {
+            if (this.totalRounds <= 0) {
+                alert("Please choose a positive number of rounds")
+            }
+            else {
+                this.gameStart = true
+
+            }
         },
         roll() {
             return Math.floor(Math.random() * 6 + 1)
